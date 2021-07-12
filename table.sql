242b76a24831340c409c290084aa92945869f22d
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-12 13:09:52 +0900

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,13 +1,36 @@
+
+-- Delete table
 TRUNCATE TABLE IF EXISTS tableName;
 DROP TABLE IF EXISTS tableName;
 
+-- Overwrite table
 INSERT OVERWRITE TABLE tableName
 SELECT *
 FROM originalTableName
 
+-- Insert data into table
 INSERT INTO TABLE tableName
 SELECT *
 FROM originalTableName
+
+-- Table information
+DESCRIBE TABLE_NAME
+DESCRIBE FORMATTED TABLE_NAME
+
+-- Create table
+CREATE TABLE tableName (
+  colName1 STRING,
+  colName2 STRING,
+  colName3 STRING,
+  colName4 STRING,
+  colName5 STRING);
+
+INSERT INTO tableName (colName1, colName2, colName3, colName4, colName5)
+VALUES
+("red", "circle", "Y", "apple", "paper"),
+("yellow", "rectangle", "N", "banana", "pencil"),
+("blue", "square", "Y", "carrot", "paper"),
+("green", "circle", "N", "melon", "paper")
 
 
 
