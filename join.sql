a2e36700647ded4439793969d6ffeda7692f53d5
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-12 11:09:57 +0900

    Create join.sql

---
+++ join.sql
@@ -0,0 +1,20 @@
+
+-- LEFT JOIN
+SELECT A.COL1
+     , A.COL2
+     , B.COL5
+FROM table1 as A
+LEFT JOIN table2 as B on a.col1 = b.col1
+
+
+-- UNION ALL
+-- All column's names should be the same
+-- table1 + table2 + ... tableN
+SELECT col1, col2, col3 FROM table1
+UNION ALL
+SELECT col1, col2, col3 FROM table2
+UNION ALL
+SELECT col1, col2, col3 FROM table3
+
+
+
