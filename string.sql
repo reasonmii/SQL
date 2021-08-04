d4d8f628226bf65963184846f2b80fca2d184123
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-04 10:35:06 +0900

    Update string.sql

--- string.sql
+++ string.sql
@@ -1,11 +1,22 @@
+/* ===========================================================
+Concatenate words with seperator
+============================================================== */
 
--- Concatenate words with seperator
 -- ex) concat_ws('-', '2021', '07', '01) -> '2021-07-01'
 concat_ws(seperator, word1, word2, word3, ...)
 
--- Replace word1 to word2
+
+/* ===========================================================
+Replace word1 to word2
+============================================================== */
+
 -- ex) REGEXP_REPLACE('2021-07-01', '-', '') -> '20210701'
 REGEXP_REPLACE(column_name, word1, word2)
+
+
+/* ===========================================================
+LEFT, RIGHT, SUBSTR
+============================================================== */
 
 -- ex) 'HELLOWORLD' -> 'HELL'
 LEFT(colName, 4)
@@ -15,6 +26,18 @@ RIGHT(colName, 4)
 
 -- ex) 'HELLOWORLD' -> 'WO'
 SUBSTR(colName, 6, 2)
+
+
+/* ===========================================================
+Fill the space
+============================================================== */
+
+-- 'HELLO' -> '**HELLO'
+SELECT LPAD('HELLO', 7, '*') AS col1
+
+-- 'HELLO' -> 'HELLO**'
+SELECT RPAD('HELLO', 7, '*') AS col1
+
 
 
 
