9fa2c31470b89955bdbf64eb0e86437fc991fb9a
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-04 10:06:33 +0900

    Create count.sql

---
+++ count.sql
@@ -0,0 +1,36 @@
+/* ===========================================================
+COUNT
+============================================================== */
+
+select column1
+     , column2
+     , column3
+     , count(*) as cnt
+     , count(distinct column4) as cnt2
+from Table
+
+
+/* ===========================================================
+SUM
+============================================================== */
+
+select column1
+     , sum(case when column2 like '%ABC%' then 1 else 0 end) as sum1
+     , sum(case when column3 in ('A', 'B') then 1 else 0 end) as sum2
+     , sum(case when column4 is not null then 1 else 0 end) as sum3
+from Table
+
+
+/* ===========================================================
+DECODE
+============================================================== */
+
+SELECT column1
+     -- if column2 is 'A' then 1 else 0
+     , sum(DECODE(column2, 'A', 1, 0)) as sum4
+     -- if column3 is NULL then 0 else 1
+     , sum(DECODE(column3, NULL, 0, 1)) as sum4
+     -- if column4 is 'A' OR 'B' OR 'C' then 1 else 0
+     , sum(DECODE(column4, 'A', 1, 'B', 1, 'C', 1, 0)) as sum3
+from Table
+
