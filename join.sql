4e61529ce23eed54ea60a8aafcdac4954ddd8877
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-04 10:19:04 +0900

    Update join.sql

--- join.sql
+++ join.sql
@@ -6,6 +6,21 @@ SELECT A.COL1
 FROM table1 as A
 LEFT JOIN table2 as B on a.col1 = b.col1
 
+-- INNER JOIN
+SELECT A.COL1
+     , A.COL2
+     , B.COL5
+FROM table1 as A
+INNER JOIN table2 as B on a.col1 = b.col1
+
+-- UNION
+-- All column's names should be the same
+-- table1 + table2 + ... tableN
+SELECT col1, col2, col3 FROM table1
+UNION
+SELECT col1, col2, col3 FROM table2
+UNION
+SELECT col1, col2, col3 FROM table3
 
 -- UNION ALL
 -- All column's names should be the same
@@ -15,6 +30,5 @@ UNION ALL
 SELECT col1, col2, col3 FROM table2
 UNION ALL
 SELECT col1, col2, col3 FROM table3
-
 
 
