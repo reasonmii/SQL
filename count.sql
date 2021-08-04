bcf1393626ce1d8bfa32c4baa5f3e9429b21dfb1
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-04 10:37:28 +0900

    Update count.sql

--- count.sql
+++ count.sql
@@ -5,7 +5,7 @@ SELECT column1
      , count(*) as cnt1
      , count(distinct column4) as cnt2
 FROM TABLE
-
+GROUP BY 1,2,3
 
 -- SUM
 SELECT column1
@@ -13,14 +13,21 @@ SELECT column1
      , sum(case when column3 in ('A', 'B') then 1 else 0 end) as sum2
      , sum(case when column4 is not null then 1 else 0 end) as sum3
 FROM TABLE
-
+GROUP BY 1
 
 -- AVG
 SELECT column1
      , AVG(column1) as avg1
      , sum(case when column3 in ('A', 'B') then 1 else 0 end) / count(*) as avg2
 FROM TABLE
+GROUP BY 1
 
+-- MAX/MIN
+SELECT column1
+     , MAX(column2) AS maxVal
+     , MIN(column2) AS minVal
+FROM TABLE
+GROUP BY 1
 
 -- DECODE
 SELECT column1
@@ -31,4 +38,5 @@ SELECT column1
      -- if column4 is 'A' OR 'B' OR 'C' then 1 else 0
      , sum(DECODE(column4, 'A', 1, 'B', 1, 'C', 1, 0)) as sum3
 FROM TABLE
+GROUP BY 1
 
