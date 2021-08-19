3b954a0e2bf3d691317d42e148441ff91a41bb1d
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-19 17:29:34 +0900

    Update null.sql

--- null.sql
+++ null.sql
@@ -4,6 +4,9 @@ NVL(ROUND(column1 / (column2 + column3)), 0) as colName
 
 IFNULL(colName, 0) as colName
 
-
+-- Print 'A'
+-- if 'A' is NULL print 'B'
+-- if 'A' and 'B' are NULL print 'C'
+COALESCE(A, B, C)
 
 
