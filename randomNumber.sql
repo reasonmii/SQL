e3d81b727de5b615bbc6a0ceb5421c666839e800
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-19 17:32:53 +0900

    Create randomNumber.sql

---
+++ randomNumber.sql
@@ -0,0 +1,8 @@
+/*
+※'IMPALA' SQL creates fixed random number, if you put the query in subquery
+   So, it's better to make temp table for the random number and refer that
+*/
+-- Create random number
+RAND(UNIX_TIMESTAMP(NOW()))
+
+
