9e52460ea90bb9472083f131b0981a60aedf8433
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-19 17:26:55 +0900

    Create where.sql

---
+++ where.sql
@@ -0,0 +1,12 @@
+
+-- The result is always 'TRUE'
+-- It's useful, because you can add more conditions below freely and easy to make them become comment
+WHERE 1=1
+AND yymm = '202101'
+AND gender = 'male'
+-- AND nation = 'Korea'
+
+-- The result is always 'FALSE'
+-- It's useful when creating table
+WHERE 1=2
+
