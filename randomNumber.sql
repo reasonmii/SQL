34981a6f24ad0c8abcf85c22d2d77b6c588e5f4c
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-19 17:34:15 +0900

    Update randomNumber.sql

--- randomNumber.sql
+++ randomNumber.sql
@@ -1,8 +1,12 @@
 /*
-※'IMPALA' SQL creates fixed random number, if you put the query in subquery
-   So, it's better to make temp table for the random number and refer that
+※'IMPALA' SQL creates fixed random number,
+if you put the query in subquery
+So, it's better to make temp table for the random number and refer that
 */
+
 -- Create random number
 RAND(UNIX_TIMESTAMP(NOW()))
 
+-- Create 'INT' random number
+row_number()over(partition by column1 order by RAND(UNIX_TIMESTAMP(NOW())))
 
