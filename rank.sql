bf390924ce82b5565187b6e222f1cc6fc91fe098
YUNA <79252563+reasonmii@users.noreply.github.com>
2022-01-09 12:34:19 +0900

    Update rank.sql

--- rank.sql
+++ rank.sql
@@ -5,8 +5,8 @@ SELECT ROW_NUMBER()OVER (PARTITION BY column1 order by column2) as ODR
 SELECT ROW_NUMBER()OVER (PARTITION BY column1 order by column2 desc) as ODR
 
 /* When you order by a field that may contain NULL values,
-   any NULLs are considered to have the lowest value.
-   So ordering in DESC order will see the NULLs appearing last. */
+ * any NULLs are considered to have the lowest value.
+ * So ordering in DESC order will see the NULLs appearing last. */
 SELECT ROW_NUMBER()OVER (PARTITION BY column1 order by column2 DESC NULLS LAST) as ODR
 SELECT ROW_NUMBER()OVER (PARTITION BY column1 order by column2 ASC NULLS LAST) as ODR
 
