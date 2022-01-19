fc553196540f7f51fba8ffa36711a5868f772247
YUNA <79252563+reasonmii@users.noreply.github.com>
2022-01-19 10:23:39 +0900

    Update where.sql

--- where.sql
+++ where.sql
@@ -1,4 +1,5 @@
 
+-- 1=1 is for?
 -- The result is always 'TRUE'
 -- It's useful, because you can add more conditions below freely and easy to make them become comment
 WHERE 1=1
@@ -6,6 +7,7 @@ AND yymm = '202101'
 AND gender = 'male'
 -- AND nation = 'Korea'
 
+-- 1=2 is for?
 -- The result is always 'FALSE'
 -- It's useful when creating table
 WHERE 1=2
