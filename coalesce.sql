cfd1032d5c0994e42ba2b5261d971d781e185dec
YUNA <79252563+reasonmii@users.noreply.github.com>
2022-07-08 19:31:23 +0900

    Delete coalesce.sql

--- coalesce.sql
+++
@@ -1,9 +0,0 @@
-
--- COALESCE(OPTION1, OPTION2, OPTION3)
--- if 'option1' is null -> option2
--- if 'option2' is null -> option3
-
-SELECT BASE_YYMM
-     , COALESCE(A.STRT_DT, A.BEGIN_DT, '999') AS DT
-FROM TABLE AS A
-
