40f7fe0f2796897b1545ff49b1bc204d7976e7fd
YUNA <79252563+reasonmii@users.noreply.github.com>
2022-07-08 19:31:19 +0900

    Update null.sql

--- null.sql
+++ null.sql
@@ -4,10 +4,12 @@ NVL(ROUND(column1 / (column2 + column3)), 0) as colName
 
 IFNULL(colName, 0) as colName
 
--- Print 'A'
--- if 'A' is NULL print 'B'
--- if 'A' and 'B' are NULL print 'C'
-COALESCE(A, B, C)
+-- COALESCE(OPTION1, OPTION2, OPTION3)
+-- if 'option1' is null -> option2
+-- if 'option2' is null -> option3
+SELECT BASE_YYMM
+     , COALESCE(A.STRT_DT, A.BEGIN_DT, '999') AS DT
+FROM TABLE AS A
 
 ORDER BY colName NULLS FIRST
 ORDER BY colName NULLS LAST
